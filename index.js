52bd536163759fb75c7e2bea8092dabf2a1a2692
April Hung <huotsai2fi@gmail.com>
2022-03-13 20:32:09 +0800

    add a smiley face

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hello world ;)');
 });
 
 app.get('/api/products', async (req, res) => {
