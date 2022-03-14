5fad2f46358d5c5b9d85cf8873f791d571cceb56
April Hung <huotsai2fi@gmail.com>
2022-03-14 18:59:20 +0800

    remove 哈哈

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('hello world ;) 哈哈哈哈');
+  res.send('hello world ;)');
 });
 
 app.get('/api/products', async (req, res) => {
