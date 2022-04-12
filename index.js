04d8b33a365a0576ed9c806c08297e3e1181b3c8
Jasonchen23 <kevin3121961@gmail.com>
2022-04-12 14:23:47 +0800

    add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import session from 'express-session';
 import fileStore from 'session-file-store';
-import swaggerUi from 'swagger-ui-express';
+// import swaggerUi from 'swagger-ui-express';
 // import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 // const require = createRequire(import.meta.url); // construct the require method
 // const swaggerDocument = require("./swagger.json") // use the require method
